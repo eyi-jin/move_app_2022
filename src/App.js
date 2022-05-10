802faa0517ebf6eebacc3ff28c163d78e264c2f4
eyi-jin <jmj2588547@naver.com>
2022-05-10 13:38:34 +0900

    화면 로딩 - 영화 앱 만들기 연습

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
 import React from "react";
 
 class App extends React.Component {
-  constructor(props){
-    super(props);
-    console.log('hello');
-  }
   state ={
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
-
-  add = () => {
-    this.setState(current => ({
-      count : current.count + 1,
-    }));
-  };
-
-  minus = () => {
-    this.setState(current => ({
-      count : current.count -1,
-    }));
-  };
-
   componentDidMount(){
-    console.log('component rendered');
-  }
-
-  componentDidUpdate(){
-    console.log('I just updated');
-  }
-
-  componentWillUnmount(){
-    console.log('Goodbye, cruel world');
+    setTimeout(() =>{
+      this.setState({isLoading: false});
+    }, 3000);
   }
   render(){
-    console.log("I'm rendering");
+    const {isLoading} = this.state;
     return(
-      <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
+      <div>{isLoading ? 'Loading...': 'We are ready'}</div>
     );
   }
 }
