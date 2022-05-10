810b4cd2c5faff4fc69d63d76d951ddf5b228220
eyi-jin <jmj2588547@naver.com>
2022-05-10 11:32:40 +0900

    컴포넌트,JSX,props - 메뉴판 연습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types';
+
+function Food({name, picture, rating}){
+  return (
+    <div>
+      <h3>I like {name}</h3>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name}/>
+    </div>
+  );
+
+}
+
+const foodILike =[
+  {
+    id:1,
+    name: 'Kimchi',
+    image: 'https://health.chosun.com/site/data/img_dir/2021/09/01/2021090100998_0.jpg',
+    rating: 5.0
+  },
+  {
+    id:2,
+    name: 'Samgyeopsal',
+    image: 'https://src.hidoc.co.kr/image/board/2021/8/27/1630049957798_0.jpg',
+    rating: 4.8
+  },
+  {
+    id:3,
+    name: 'Bibimbap',
+    image: 'http://img.danawa.com/prod_img/500000/167/890/img/13890167_1.jpg?shrink=330:330&_v=20210517095810',
+    rating: 4.5
+  }
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
+      ))}
     </div>
   );
 }
+
+Food.propTypes ={
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+
+};
 
 export default App;
