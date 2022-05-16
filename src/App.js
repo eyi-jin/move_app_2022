1bef1934279dcce7e0c465152eff8b9d027adef0
eyi-jin <jmj2588547@naver.com>
2022-05-16 21:31:38 +0900

    꾸미기-영화앱 동적으로 만들기

--- src/App.js
+++ src/App.js
@@ -1,51 +1,11 @@
-import axios from "axios";
-import React from "react";
-import Movie from './Movie';
+import React from 'react';
 import './App.css';
+import Home from './routes/Home';
 
-class App extends React.Component {
-  state ={
-    isLoading: true,
-    movies: [],
-  };
-
-  getMovies = async () => {
-    const {
-      data: {
-        data:{movies},
-      },
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
-    this.setState({movies, isLoading: false});
-  };
-  componentDidMount(){
-    this.getMovies();
-  }
-  render(){
-    const {isLoading, movies } = this.state;
-    return(
-      <section class="container">
-        {isLoading ?(
-          <div class="loader">
-            <span class="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div class ="movies">
-            {movies.map(movie=> (
-                <Movie 
-                  key={movie.id}
-                  id={movie.id}
-                  title = {movie.title}
-                  year = {movie.year}
-                  summary = {movie.summary}
-                  poster= {movie.medium_cover_image}
-                  genres= {movie.genres}
-                />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <Home />
+  );
 }
 
 export default App;
