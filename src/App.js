ba17fad1a05975ffeda06e838b49adad802592ba
eyi-jin <jmj2588547@naver.com>
2022-06-05 19:38:28 +0900

    네비게이션 만들기 - Home, About

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import './App.css';
-import Home from './routes/Home';
+import React from "react";
+import "./App.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <Home />
+    <HashRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" exact={true} element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
